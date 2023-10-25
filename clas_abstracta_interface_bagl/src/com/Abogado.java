7eec029869d940384f5f7f2304851d22a9298b31
BrianAGarciaL <abrngl00@gmail.com>
2023-10-25 10:13:05 -0600

    Se agregó carpeta de interfaces

---
+++ clas_abstracta_interface_bagl/src/com/Abogado.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Abogado extends Profesionista{
+
+	public Abogado() {
+		
+	}
+
+	
+	
+	@Override
+	public void trabajar() {
+
+		System.out.println("Asesorar, leer, documentar, mentir, ...");
+		
+	}
+	
+}
---
+++ clas_abstracta_interface_bagl/src/com/ILeyes.java
@@ -0,0 +1,10 @@
+package com;
+
+public interface ILeyes {
+
+	void realizarContratos();
+	void interpretarLeyes();
+	void notificar();
+	void defenderDerechos();
+	
+}
