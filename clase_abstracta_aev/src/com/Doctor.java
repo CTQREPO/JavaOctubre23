e984a8b87670fb14903e840eac4f974dc1456b3e
AdryEVT <arianespinoza77@gmail.com>
2023-10-25 10:11:34 -0600

    Primer commit Abstracta

---
+++ clase_abstracta_aev/src/com/Doctor.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Doctor extends Profesionista {
+	
+	public Doctor() {
+		//Este constructor vacio sirve para instanciar a la clase
+	}
+
+	@Override
+	public void trabajar() {
+		System.out.println("Consultas, expide recetas, operar, diagnosticos");
+		
+	}
+	
+	
+
+}
