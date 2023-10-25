a1c076a9545b8b85769360ad767c30a7110d1082
Badchiquipack <miguel.arriaga.arellano@gmail.com>
2023-10-25 10:15:09 -0600

    clase abstracta

---
+++ claseabstracta-maaa/src/com/Periodista.java
@@ -0,0 +1,67 @@
+package com;
+
+public class Periodista extends Profesionista implements IMedicina,ILeyes{
+
+	public Periodista() {
+		
+	}
+	
+	
+	@Override
+	public void trabajar() {
+		System.out.println("grabar");
+		
+	}
+	
+	//Metodos implementados (Comportamiento forma 3)
+
+
+	@Override
+	public void hacerRCP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void inyectar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void medirPresion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void presentarCasos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void interpretarLeyes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void elaborarDemandas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void redactarContratos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
