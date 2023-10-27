0873bacc6684e7a0336c0f7fcb1bb60741f730e1
JuanCarlosOrea97 <juancarlosog97@gmail.com>
2023-10-27 10:23:33 -0600

    proyecto cajero jcog

---
+++ Colecciones_jcog/src/com/IOperaciones.java
@@ -0,0 +1,16 @@
+package com;
+
+import java.util.List;
+
+public interface IOperaciones {
+
+	//deposito, retiro, transferencia
+	
+	
+	Ticket depositar(Cuenta destino,double monto);
+	List<Object> retirar(Cuenta origen,double monto);
+	Ticket transferir(Cuenta origen, Cuenta destino,double monto);
+	
+	
+	
+}
